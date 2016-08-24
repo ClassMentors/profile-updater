13e2a33d039255a38a4228a08e5d47fbbf877528
Chris Boesch <cboesch@smu.edu.sg>
2016-08-24 16:04:42 +0800

    Adding fcc all achievements downloader

--- test/cm-worker.spec.js
+++ test/cm-worker.spec.js
@@ -17,6 +17,10 @@ worker.initiateFirebase("http://home.com", "1234");
 // data is logged
 
 describe("Worker", function() {
+    it("can parse fcc profile", function() {
+        //expect(true).to.equal(false);
+        //expect(worker.get_service_url("BAD", "chris")).to.equal("");
+    });
     it("gets service url", function() {
         //expect(worker.get_service_url("codeCombat", "chris")).to.equal("http://codecombat.com/db/user/chris/level.sessions?project=state.complete,levelID,levelName");
         expect(worker.get_service_url("codeSchool", "chris")).to.equal("https://www.codeschool.com/users/chris.json");
@@ -24,13 +28,17 @@ describe("Worker", function() {
         expect(worker.get_service_url("BAD", "chris")).to.equal("");
     });
     it("processes service responses", function() {
+        //Todo - update to support new parser. 
         expect(worker.get_achievements_from_response("freeCodeCamp", ">[ 9 ]<")).to.equal('9');
         expect(worker.get_achievements_from_response("freeCodeCamp", ">[ 400 ]<")).to.equal('400');
+        
         expect(worker.get_achievements_from_response("codeCombat", "[]")).to.equal(0);
         expect(worker.get_achievements_from_response("codeCombat", '[{"state":{"complete":true}}]')).to.equal(1);
         expect(worker.get_achievements_from_response("codeSchool", '{"badges":[]}')).to.equal(0);
     });
     
+    //Todo: Check for bad fccProfiles. Update to -1 rather than <!. Duplicate error first. 
+
     //Todo: Add tests
     it("update achievements", function() {
         var location = "classMentors/userAchievements/chris/services/codeCombat";
